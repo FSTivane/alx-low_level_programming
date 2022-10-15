85ef1bf97f9bf79a58508af67d90bbdc77c9cd91
FSTivane <ciscotivane@gmail.com>
2022-10-15 08:45:57 -0700

    print lowercase except q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet excepf q.
+ *
+ * Return: Alwaws o.
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+
+		if(l != 'q')
+
+			putchar(l);
+
+	putchar('\n');
+
+	return(0);
+
+}
+
