eb3fad83e3550638cb31872a2f935e242e4fd329
FSTivane <ciscotivane@gmail.com>
2022-10-19 13:59:03 -0700

    Print a absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * _abs - print a absolute value of integer
+ *
+ * @c: is the argument
+ * Return: 0
+*/
+
+int _abs(int);
+{
+	if (c > 0 || c == 0)
+
+	{
+
+	return (c);
+
+	}
+
+	else
+
+	return (c * -1);
+
+}
