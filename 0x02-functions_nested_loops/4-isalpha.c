c96971f18498c78f421a180832aaee1010c9a3f1
FSTivane <ciscotivane@gmail.com>
2022-10-19 12:12:03 -0700

    Check wether is a letter, lower or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check wether is a letter, lower or uppercase
+ *
+ *@c:is the argument to be used in the function
+ *
+ * Return: 1 if success and zero if else
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a') && (c <= 'z'))
+		{
+			return (1);
+		}
+		else
+		return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int _putchar(char l);
 void print_alphabet(char l);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 #endif /* _MAIN_H_ */
