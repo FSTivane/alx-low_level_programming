b00cf2ea3c7c6580444557e05999080a92997771
FSTivane <ciscotivane@gmail.com>
2022-10-23 09:26:09 -0700

    Print every minute for a daywq

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,49 +1,40 @@
 #include "main.h"
 
 /**
- * prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
  *
  * minutes
  * Return (0)
 */
 
 void jack_bauer(void)
-
 {
-
 	int hours = 0;
-
 	int minutes = 0;
-
 	int hours_remainder;
-
 	int mins_remainder;
 
-
-
 	while (hours <= 23)
-
 	{
 
 	while (minutes <= 59)
-
 	{
 
-	mins_remainder = minutes % 10;
+		mins_remainder = minutes % 10;
 
-	hours_remainder = hours % 10;
+		hours_remainder = hours % 10;
 
-	_putchar(hours / 10 + '0');
+		_putchar(hours / 10 + '0');
 
-	_putchar(hours_remainder + '0');
+		_putchar(hours_remainder + '0');
 
-	_putchar(':');
+		_putchar(':');
 
-	_putchar(minutes / 10 + '0');
+		_putchar(minutes / 10 + '0');
 
-	_putchar(mins_remainder + '0');
+		_putchar(mins_remainder + '0');
 
-	minutes++;
+		minutes++;
 
 	_putchar('\n');
 
@@ -52,7 +43,5 @@ void jack_bauer(void)
 	hours++;
 
 	minutes = 0;
-
 	}
-
 }
