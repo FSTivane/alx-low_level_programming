faecee082550b8b7824a1b82a6cac36ac7807d17
FSTivane <ciscotivane@gmail.com>
2023-08-17 00:04:12 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - check uppercases chars
+ *
+ * Return:1 or 0
+ */
+
+int c;
+int _isupper(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+	int _isupper(int c);
+
+#endif
