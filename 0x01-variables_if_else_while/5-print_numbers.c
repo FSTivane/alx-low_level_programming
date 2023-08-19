e1b4fd7bd51e2576553c4f7fb11fcf43cf438ced
FSTivane <ciscotivane@gmail.com>
2023-08-18 22:17:57 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: o
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+
+
+	return (0);
+}
