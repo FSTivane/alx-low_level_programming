a76b825c9636a4fac04c14fa4a856cf3aef782be
FSTivane <ciscotivane@gmail.com>
2023-08-19 01:28:16 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of 2 2 gigits
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		int first_digit_i = i / 10;
+		int second_digit_i = i % 10;
+		
+		for (j = i + 1; j < 100; j++)
+		{
+			int first_digit_j = j / 10;
+			int second_digit_j = j % 10;
+
+			if ((first_digit_i != first_digit_j || second_digit_i != second_digit_j) &&
+                	(first_digit_i != second_digit_j && second_digit_i != first_digit_j))
+				{
+				putchar('0' + first_digit_i);
+				putchar('0' + second_digit_i);
+				putchar(' ');
+				putchar('0' + first_digit_j);
+				putchar('0' + second_digit_j);
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
