432d0b924a0bf277342c84586cf79a23e50684b7
FSTivane <ciscotivane@gmail.com>
2023-08-19 01:36:20 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,23 +14,23 @@ int main(void)
 	{
 		int first_digit_i = i / 10;
 		int second_digit_i = i % 10;
-	
+
 		for (j = i + 1; j < 100; j++)
 		{
 			int first_digit_j = j / 10;
 			int second_digit_j = j % 10;
 
-				putchar('0' + first_digit_i);
-				putchar('0' + second_digit_i);
-				putchar(' ');
-				putchar('0' + first_digit_j);
-				putchar('0' + second_digit_j);
+			putchar('0' + first_digit_i);
+			putchar('0' + second_digit_i);
+			putchar(' ');
+			putchar('0' + first_digit_j);
+			putchar('0' + second_digit_j);
 
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
