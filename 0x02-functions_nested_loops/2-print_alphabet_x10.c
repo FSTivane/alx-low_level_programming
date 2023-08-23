567646476a9612084c0f6cf98fb6b5a86070cbd0
FSTivane <ciscotivane@gmail.com>
2023-08-23 13:34:35 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,15 @@
 void print_alphabet_x10(void)
 {
 
-	int i = 0;
+	int i;
 	char l;
 
 	for (i = 0; i < 9; i++)
 	{
 		for (l = 'a'; l <= 'z'; l++)
+		{
 			_putchar(l);
+		}
 			_putchar('\n');
 	}
 }
