3b53caf7f805e4b936d8693409fc31756f67c833
FSTivane <ciscotivane@gmail.com>
2023-08-23 11:37:53 -0700

    3. islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lowercase
+ * print_alphabet_x10 - prints alphabet 10x
  *
  * Return: 0
  */
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 9; i++)
 	{
 		for (l = 'a'; l <= 'z'; l++)
-			_putchar(i);
+			_putchar(l);
 	}
 		_putchar('\n');
 
