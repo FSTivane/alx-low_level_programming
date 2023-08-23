76137a926c783ba163ee9d2013e434ac1d0de0a1
FSTivane <ciscotivane@gmail.com>
2023-08-23 13:40:19 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - two numbers
+ *@x: argumrnt
+ *@y: argumrnt
+ * Return: result
+*/
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
+
