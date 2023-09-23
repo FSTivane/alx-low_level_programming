1bb5e6834643dfb75449c7fc5a2540f600b29ee8
FSTivane <ciscotivane@gmail.com>
2023-09-23 15:02:47 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	int num1 = atoi(argv[1]);
+	char *operator = argv[2];
+	int num2 = atoi(argv[3]);
+
+	int (*op_func)(int, int) = get_op_func(operator);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	int result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -5,3 +5,5 @@
 1.  executes a function given as a parameter on each element of an array.
 
 2.  searches for an integer
+
+3. A goal is not always meant to be reached, it often serves simply as something to aim at
