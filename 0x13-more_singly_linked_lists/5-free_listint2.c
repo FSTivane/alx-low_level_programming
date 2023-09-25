411186770bb063faa57fcca3687af61cdb8d9dc6
FSTivane <ciscotivane@gmail.com>
2023-09-25 07:03:12 -0700

    6. Pop

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,22 +5,23 @@
  * free_listint2 - Frees a listint_t list and sets the head to NULL
  * @head: Pointer to the pointer to the head of the list
  */
+
 void free_listint2(listint_t **head)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (head == NULL || *head == NULL)
-        return; 
+	if (head == NULL || *head == NULL)
+	return;
 
-    current = *head;
+	current = *head;
 
-    while (current != NULL)
-    {
-        temp = current->next;
-        free(current);
-        current = temp;
-    }
+	while (current != NULL)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
 
-    *head = NULL;
+	*head = NULL;
 }
 
