13af7dfd4344129ec5ded01a1a4dd0a113b73913
FSTivane <ciscotivane@gmail.com>
2023-11-11 03:53:04 -0800

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,12 @@
 #include  "function_pointers.h"
+
 /**
- * int_index - return index place if comparison = true, else -1
- * @array: array
- * @size: size of elements in array
- * @cmp: pointer to func of one of the 3
+ *int_index - return index place if comparison
+ *@array: array
+ *@size: size of elements in array
+ *@cmp: pointer to func of one of the 3
  *
+ * Return: -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
